16f0947bf4c0574d49739b67625c5d0031dff4e5
sabyathak <sabyathak22@gmail.com>
2019-07-15 13:27:59 -0700

    Inital COmmit

--- websites/ViewController.swift
+++ websites/ViewController.swift
@@ -15,6 +15,12 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func youtubePressed(_ sender: Any) {
+        UIApplication.shared.open(URL(string:"https://www.youtube.com/watch?v=cKlTOBcbNDA")! as URL, options : [:], completionHandler: nil)
+    }
+    
+    @IBAction func harry(_ sender: Any) {
+        UIApplication.shared.open(URL(string: "https://www.instagram.com/harrystyles/?hl=en")! as URL, options : [:], completionHandler: nil)
+    }
 }
 
